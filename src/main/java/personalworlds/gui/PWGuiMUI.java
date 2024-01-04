b322e21f3d37a4aa1b35c577e4662e7a5fb927ff
Irgendwer01 <67506320+Irgendwer01@users.noreply.github.com>
2024-01-04 21:42:21 +0100

    Update PWGuiMUI.java

--- src/main/java/personalworlds/gui/PWGuiMUI.java
+++ src/main/java/personalworlds/gui/PWGuiMUI.java
@@ -7,38 +7,50 @@ import com.cleanroommc.modularui.drawable.ItemDrawable;
 import com.cleanroommc.modularui.drawable.UITexture;
 import com.cleanroommc.modularui.screen.ModularPanel;
 import com.cleanroommc.modularui.screen.ModularScreen;
+import com.cleanroommc.modularui.screen.viewport.GuiContext;
+import com.cleanroommc.modularui.theme.WidgetTheme;
+import com.cleanroommc.modularui.utils.Alignment;
+import com.cleanroommc.modularui.widget.WidgetTree;
 import com.cleanroommc.modularui.widgets.ButtonWidget;
 import com.cleanroommc.modularui.widgets.ListWidget;
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.gen.FlatLayerInfo;
 import personalworlds.PWConfig;
+import personalworlds.PersonalWorlds;
 import personalworlds.blocks.tile.TilePersonalPortal;
+import personalworlds.world.DimensionConfig;
 
 import java.util.ArrayList;
+import java.util.function.Consumer;
 
 public class PWGuiMUI {
 
-    private TilePersonalPortal TPP;
-
-    public PWGuiMUI(TilePersonalPortal TPP) {
-    this.TPP = TPP;
-    }
-    private ArrayList<IWidget> list = new ArrayList<>();
-    private ArrayList<ItemStack> itemStacks = new ArrayList<>();
+    private ArrayList<IWidget> biomeList = new ArrayList<>();
     private ArrayList<IWidget> blockList = new ArrayList<>();
+    private ListWidget widget;
+    private final DimensionConfig dimensionConfig = new DimensionConfig();
 
 
     public ModularScreen createGUI() {
         for (IBlockState blockState : PWConfig.getAllowedBlocks()) {
             Block block = blockState.getBlock();
-            int meta = blockState.getBlock().damageDropped(blockState);
-            ItemStack stack = new ItemStack(block, 1, meta);
-            blockList.add(new ButtonWidget<>().size(20, 20).overlay(new ItemDrawable(stack)).addTooltipLine(stack.getDisplayName()));
+            int itemMeta = block.damageDropped(blockState);
+            int meta = block.getMetaFromState(blockState);
+            ItemStack stack = new ItemStack(block, 1, itemMeta);
+            blockList.add(new ButtonWidget<>().size(15, 15)
+                    .overlay(new ItemDrawable(stack))
+                    .addTooltipLine(stack.getDisplayName())
+                    .onMousePressed(i -> {
+                        dimensionConfig.getLayers().add(new FlatLayerInfo(3, 1, block, meta));
+                        redrawLayers();
+                       return true;
+                    }));
         }
         for (Biome biome : PWConfig.getAllowedBiomes()) {
-            list.add(new ButtonWidget<>().size(90, 20).overlay(IKey.str(biome.getBiomeName())));
+            biomeList.add(new ButtonWidget<>().size(85, 15).overlay(IKey.str(biome.getBiomeName())));
         }
 
         ModularPanel panel = ModularPanel.defaultPanel("PWGUI");
@@ -58,22 +70,38 @@ public class PWGuiMUI {
                     return true;
                 }));
         panel.child(new ButtonWidget<>()
-                .size(20, 20)
+                .size(15, 15)
                 .bottom(90).left(9)
                 .overlay(UITexture.builder().imageSize(16, 16).location("personalworlds", "checkmark").build()));
         panel.child(new ButtonWidget<>()
-                .size(20, 20)
+                .size(15, 15)
                 .bottom(65).left(9)
                 .overlay(UITexture.builder().imageSize(16, 16).location("personalworlds", "checkmark").build()));
-        panel.child(new ListWidget<>(list)
+        panel.child(new ListWidget<>(biomeList)
                         .bottom(40).left(9)
-                        .size(90, 20));
-        panel.child(IKey.str("Trees").asWidget()
-                .bottom(105).left(35));
+                        .size(85, 15));
         panel.child(new ListWidget<>(blockList)
                 .top(9).right(60)
-                .size(20, 150));
+                .size(15, 150));
+        widget = new ListWidget<>()
+                .top(9).right(20)
+                .size(15, 150);
+        panel.child(widget);
         return new ModularScreen(panel);
+    }
 
+    private void redrawLayers() {
+        for (FlatLayerInfo flatLayerInfo : dimensionConfig.getLayers()) {
+            IBlockState blockState = flatLayerInfo.getLayerMaterial();
+            Block block = flatLayerInfo.getLayerMaterial().getBlock();
+            int itemMeta = block.damageDropped(blockState);
+            ItemStack stack = new ItemStack(block, 1, itemMeta);
+            int count = widget.getValues().size();
+            widget.getValues().clear();
+            widget.add(new ButtonWidget<>().size(15, 15)
+                    .overlay(new ItemDrawable(stack))
+                    .addTooltipLine(stack.getDisplayName()), count);
+        }
+        WidgetTree.resize(widget);
     }
 }
