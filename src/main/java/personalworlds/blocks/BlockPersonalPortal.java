70c07c096fbc94ee19c1f29a7fa5dcf13fad57fe
Irgendwer01 <67506320+Irgendwer01@users.noreply.github.com>
2024-01-20 13:43:21 +0100

    Do not do anything while player is riding

--- src/main/java/personalworlds/blocks/BlockPersonalPortal.java
+++ src/main/java/personalworlds/blocks/BlockPersonalPortal.java
@@ -60,7 +60,7 @@ public class BlockPersonalPortal extends Block implements ITileEntityProvider {
     @Override
     public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn,
                                     EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
-        if (!worldIn.isRemote) {
+        if (!worldIn.isRemote && !playerIn.isRiding()) {
             TileEntity te = worldIn.getTileEntity(pos);
             if (te instanceof TilePersonalPortal tpp) {
                 if (tpp.isActive() && !playerIn.isSneaking()) {
